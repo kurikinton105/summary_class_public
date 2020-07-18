7a84c59e9704bb590644784b9a04ad5d0b7f55c5
kurikinton105 <yamadakenta0101@gmail.com>
2020-07-18 21:14:13 +0900

    重要語の参考文献への追加

--- application.py
+++ application.py
@@ -10,87 +10,26 @@ def odd_even():
         <H1>おそらく地球を侵略しにきた！！！！<H1>
         <H1>なんかいい感じの開発して世界を征服するぞ！！！！！<H1>
         （ごめんなさい...ふざけました...）
+        開発者yamaより
         """
 
 @app.route("/", methods=["GET", "POST"])
 def main_page():
-    #return render_template("make-class-easy.html")
-
     if request.method == 'GET':
-        print("GET")
-        #text = request.args.get("input_text")
+        #print("GET")
         text = "ここに結果が出力されます"
-        return render_template("make-class-easy.html",text=text)
+        data_word = [" "]
+        val = False
+        return render_template("make-class-easy.html",text=text,data_word=data_word,val = val)
     elif request.method == 'POST':
-        print("POST")
+        #print("POST")
+        val = True #フラグを１にする
         text = request.form["input_text"]
-        #print(text)
-        #return render_template("make-class-easy.html",text=text)
-
         try:
-            result = bag_of_words_sum(str(text),50,100)
-            #print(result)
-            return render_template("make-class-easy.html",text=result)
+            result,data_word = bag_of_words_sum(str(text),50,100)
+            return render_template("make-class-easy.html",text=result,data_word=data_word,val = val)
         except:
             return render_template("make-class-easy.html",text="Error:文章は２文以上にしてください。もう一度文章を入力してください")
-
-
-@app.route("/class", methods=["GET", "POST"])
-def web():
-    if request.method == "GET":
-        #WEBリンクを代入
-        return """
-        <H1>bag-of-wordsを使った要約アプリ</H1>
-        2文以上の文章を入力してください
-        <form action="/" method="POST">
-        <input name="text"></input>
-        </form>"""
-    else:
-        try:
-            result = bag_of_words_sum(str(request.form["text"]),50,100)
-            return """
-            <H1>bag-of-wordsを使った要約アプリ</H1>
-            2文以上の文章を入力してください
-            <form action="/" method="POST">
-            <input name="text"></input>
-            </form>
-            {}<br>""".format(result)
-        except:
-            return """
-                <H1>bag-of-wordsを使った要約アプリ</H1>
-                文章は２文以上にしてください。もう一度文章を入力してください
-                <form action="/" method="POST">
-                <input name="text"></input>
-                </form>"""
-
-@app.route("/web", methods=["GET", "POST"])
-def web2():
-    if request.method == "GET":
-        #WEBリンクを代入
-        return """
-        <H1>参考文献を自動生成してくれるチートツールを作りたかった</H1>
-        URLを入力してください
-        <form action="/" method="POST">
-        <input name="url"></input>
-        </form>"""
-    else:
-        try:
-            result = scraping(str(request.form["url"]))
-            return """
-            <H1>参考文献を自動生成してくれるチートツールを作りたかった</H1>
-            URLを入力してください
-            <form action="/" method="POST">
-            <input name="url"></input>
-            </form>
-            {}<br>""".format(result[0])+"""{}""".format(result[1])
-        except:
-            return"""
-            <H1>参考文献を自動生成してくれるチートツールを作りたかった</H1>
-            URLが間違っています<br>
-            URLを入力してください
-            <form action="/" method="POST">
-            <input name="url"></input>
-            </form>"""
 
 ## おまじない
 if __name__ == "__main__":
