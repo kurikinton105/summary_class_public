08d277a89331581290e3cd29921deb8bbc45105a
tomsoya <tom_soyaS@outlook.com>
2020-07-19 19:19:53 +0900

    Developページの追加

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template,request #追加
 from bag_of_words import bag_of_words_sum
 from MakeClassEasy import MakeClassEasy
+import os
 app = Flask(__name__)
-
 @app.route("/cos5year", methods=["GET", "POST"])
 def odd_even():
     if request.method == "GET":
@@ -32,6 +32,7 @@ def main_page():
         except:
             return render_template("make-class-easy.html",text="Error:文章は２文以上にしてください。もう一度文章を入力してください")
 
+
 @app.route("/develop", methods=["GET", "POST"])
 def develop_page():
     if request.method == 'GET':
@@ -39,14 +40,14 @@ def develop_page():
         text = "ここに結果が出力されます"
         data_word = [" "]
         val = False
-        return render_template("make-class-easy-develop.html",text=text,data_word=data_word,val = val)
+        return render_template("make-class-easy-develop.html",text=text,data_explain=data_word,val = val)
     elif request.method == 'POST':
         #print("POST")
         val = True #フラグを１にする
         text = request.form["input_text"]
         try:
-            summary,explain,time = MakeClassEasy(str(text),50,100)
-            return render_template("make-class-easy-develop.html",text=summary,data_explain=explain,val = val,process_time=time)
+            result,explains,time = MakeClassEasy(str(text),50,100)
+            return render_template("make-class-easy-develop.html",text=result,data_explain=explains,val = val,process_time = time)
         except:
             return render_template("make-class-easy-develop.html",text="Error:文章は２文以上にしてください。もう一度文章を入力してください")
 
